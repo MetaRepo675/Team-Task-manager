2fe33540b429752235e887aa2b2136daefa4fdb2
MetaRepo675 <setayeh710@gmail.com>
2026-02-11 16:56:44 +0330

    Create taskpolicy.php

---
+++ app/http/request/task/app/http/resources/app/policies/taskpolicy.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\Task;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class TaskPolicy
+{
+    use HandlesAuthorization;
+
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    public function view(User $user, Task $task): bool
+    {
+        return $user->id === $task->user_id || $user->id === $task->assigned_to;
+    }
+
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    public function update(User $user, Task $task): bool
+    {
+        return $user->id === $task->user_id;
+    }
+
+    public function delete(User $user, Task $task): bool
+    {
+        return $user->id === $task->user_id;
+    }
+}
